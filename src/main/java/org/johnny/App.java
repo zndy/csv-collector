91f15272b6f71b0ed677f8bc2f07dae5c9156f1e
z.xin <z.xin@WKS-XIN>
2019-03-06 16:16:48 +0100

    version 1

--- src/main/java/org/johnny/App.java
+++ src/main/java/org/johnny/App.java
@@ -8,14 +8,6 @@ import java.util.stream.Stream;
 public class App {
     public static void main(String[] args) throws Exception {
         CsvCollector csvCollector = new CsvCollector();
-        System.out.println("JarPath: " + csvCollector.getCurrentWorkingDir());
-        Stream<Path> allFiles = csvCollector.findAllCsvFiles(csvCollector.getCurrentWorkingDir());
-        Path path = Paths.get(csvCollector.getCurrentWorkingDir() + "\\sum.txt");
-        StringBuilder sb = new StringBuilder();
-        allFiles.forEach(f -> {
-            sb.append(f);
-            sb.append("\n");
-        });
-        Files.write(path, sb.toString().getBytes());
+        csvCollector.collectToFile(csvCollector.getCurrentWorkingDir());
     }
 }
--- src/main/java/org/johnny/CsvCollector.java
+++ src/main/java/org/johnny/CsvCollector.java
@@ -1,12 +1,11 @@
 package org.johnny;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class CsvCollector {
@@ -21,7 +20,36 @@ public class CsvCollector {
     }
 
     public Stream<Path> findAllCsvFiles(String folderStr) throws IOException {
-        return findAllFiles(folderStr).filter(f -> f.toString().endsWith(".jar"));
+        return findAllFiles(folderStr).filter(f -> f.toString().endsWith(".csv"));
+    }
+
+    public String concatScvFiles(String folderStr) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        Stream<Path> allCsvFiles = findAllCsvFiles(folderStr);
+
+        AtomicInteger index = new AtomicInteger(0);
+        allCsvFiles.forEach(csv -> {
+            try {
+                List<String> allLines = Files.readAllLines(csv);
+                if (index.getAndIncrement() == 0) {
+                    sb.append(allLines.get(0));
+                    sb.append("\n");
+                }
+                sb.append(allLines.get(1));
+                sb.append("\n");
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        });
+        return sb.toString();
+    }
+
+    public void collectToFile(String folderStr) throws IOException {
+        Path resultPath = Paths.get(folderStr + "\\result.csv");
+        Files.deleteIfExists(resultPath);
+
+        String result = concatScvFiles(folderStr);
+        Files.write(resultPath, result.getBytes());
     }
 
 }
